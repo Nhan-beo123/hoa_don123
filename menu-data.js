06ad61e4f79451d76ea253c10f2d61c92b6e2d2d
Nhân Ngô - LT <nhanbeothcs@gmail.com>
2025-05-15 21:08:04 +0700

    Add files via upload

---
+++ menu-data.js
@@ -0,0 +1,7 @@
+const detectedFoods = [
+  { name: "canh cải", price: 10000 },
+  { name: "gà chiên", price: 20000 },
+  { name: "trứng chiên", price: 10000 },
+  { name: "Đậu hũ sốt cà", price: 12000 }
+  { name: "Rau muống xào", price: 12000 }
+];
